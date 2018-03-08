b76badd034a8c265802a58b1192000b719d7f798
Eros <george.marr.cobc@gmail.com>
2018-03-08 19:22:00 +0000

    init

---
+++ cmake-build-debug/CMakeFiles/Checkers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Checkers.dir/main.c.obj"
+  "Checkers.pdb"
+  "Checkers.exe"
+  "Checkers.exe.manifest"
+  "libCheckers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Checkers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
