29f1f4f829c9e77db6c689f9d86e588945780c4c
Eros <george.marr.cobc@gmail.com>
2018-03-09 11:58:25 +0000

    handles main shit

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "game.h"
 
 int main() {
+    printf("Welcome to chess!");
+    printf("Please enter a command:\n Play\nQuit");
 
+    char command;
+
+    while(scanf(" %c", &command) != 0){
+        if(command == 'p'){
+            printf("Starting game!");
+            break;
+            startGame();
+        } else if(command == 'quit'){
+            return 0;
+        } else {
+            printf("Invalid command!");
+        }
+    }
     return 0;
 }
