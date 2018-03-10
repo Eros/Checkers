1c49a58550db9daebf6c005ff4fcc8e313733ce5
Eros <george.marr.cobc@gmail.com>
2018-03-10 11:48:16 +0000

    adding in my debug tool which is like broken af but fuck it

--- cmake-build-debug/CMakeFiles/Checkers.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Checkers.dir/DependInfo.cmake
@@ -1,6 +1,7 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "C"
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
@@ -11,6 +12,14 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/C++/Projects/Checkers/debughandler/auth.cpp" "F:/C++/Projects/Checkers/cmake-build-debug/CMakeFiles/Checkers.dir/debughandler/auth.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/Checkers.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Checkers.dir/cmake_clean.cmake
@@ -1,6 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Checkers.dir/main.c.obj"
   "CMakeFiles/Checkers.dir/game.c.obj"
+  "CMakeFiles/Checkers.dir/debughandler/auth.cpp.obj"
   "Checkers.pdb"
   "Checkers.exe"
   "Checkers.exe.manifest"
@@ -8,6 +9,6 @@ file(REMOVE_RECURSE
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang C CXX)
   include(CMakeFiles/Checkers.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
